a30c1c9ca1c990c6b011f404d825bb68c24a3587
Anna Guerrero <acg2176@columbia.edu>
2020-03-19 05:49:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num.modulo(3) == 0 && num.modulo(5) == 0
+  if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
-  elsif num.modulo(3) == 0
+  elsif num % 3 == 0
     "Fizz"
-  elsif num.modulo(5) == 0
+  elsif num % 5 == 0
     "Buzz"
 end
 
